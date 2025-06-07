469693db5759a0f1bb37c8c6cba8403a099b70b9
YogeshKumar995 <1413002yogesh10206@gmail.com>
2025-06-07 12:25:27 +0530

    Set right path in Profile/index.js

--- frontend/src/components/components/Profile/Profile.jsx
+++ frontend/src/components/components/Profile/Profile.jsx
@@ -1,8 +1,6 @@
 import { ProfileHeader, ProfilePost } from "."
 
 export function Profile({isCurrentUser, profileData}){
-    
-
     return(
         <div className="flex flex-col justify-center items-center px-2 sm:px-3 lg:px-6 xl:px-8">
             <ProfileHeader 
--- frontend/src/components/components/Profile/index.js
+++ frontend/src/components/components/Profile/index.js
@@ -1,9 +1,9 @@
-import { Profile } from "./profile";
 import { ProfileHeader } from "./ProfileHeader";
 import { ProfilePost } from "./ProfilePosts";
 import { ToggleFollowBtn } from "./ToggleFollowBtn";
 import { ProfileFollowers } from "./ProfileFollowers";
 import {ProfileFollowingList} from "./ProfileFollowingList"
 import { SingleProfile } from "./SingleProfile";
+import { Profile } from "./Profile";
 
 export {Profile, ProfileHeader, ProfilePost, ToggleFollowBtn, SingleProfile, ProfileFollowers, ProfileFollowingList}
